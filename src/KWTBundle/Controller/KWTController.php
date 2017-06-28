6262f26247ab1092d176328797da39ffc76b7c4f
asauty <sauty.aymeric@gmail.com>
2017-06-28 18:04:23 +0200

    Récupération API
    Données membres

--- src/KWTBundle/Controller/KWTController.php
+++ src/KWTBundle/Controller/KWTController.php
@@ -17,6 +17,16 @@ class KWTController extends Controller
     public function membreAction()
     {
     	//Récupération de données via API Blizzard
-        return $this->render('KWTBundle:Site:membre.html.twig');
+        $url = "https://eu.api.battle.net/wow/guild/Hyjal/KWT?fields=members&locale=fr_FR&apikey=gdqbrkpkzk4sbrs7ku3h2gxqwrax22zg";
+    	$raw = file_get_contents($url);
+    	$response = json_decode($raw);
+    	foreach ($response->members as $members) 
+    	{
+    		$name[] = utf8_decode($members->character->name);
+    		$classe[] = utf8_decode($members->character->class);
+    		$role[] = utf8_decode($members->character->spec->role);
+    		$thumbnail[] = utf8_decode($members->character->thumbnail);
+    	}
+        return $this->render('KWTBundle:Site:membre.html.twig', array('name'=>$name, 'classe'=>$classe, 'role'=>$role, 'thumbnail'=>$thumbnail));
     }
 }
