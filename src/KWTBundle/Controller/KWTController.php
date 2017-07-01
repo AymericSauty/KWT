24068bed30e56d601a9bdd7d159f2dbc267dc9f1
asauty <sauty.aymeric@gmail.com>
2017-07-01 14:34:09 +0200

    Recupération data personnage
    v 0.1

--- src/KWTBundle/Controller/KWTController.php
+++ src/KWTBundle/Controller/KWTController.php
@@ -35,4 +35,57 @@ class KWTController extends Controller
     	}
         return $this->render('KWTBundle:Site:membre.html.twig',array ('characters'=>$characters , 'active'=>$active));
     }
+
+    public function characterAction($character)
+    {
+    	$active = "Personnage";
+    	//Récupération de données via API Blizzard
+    	$url = "https://eu.api.battle.net/wow/character/Hyjal/".$character."?fields=items&locale=fr_FR&apikey=gdqbrkpkzk4sbrs7ku3h2gxqwrax22zg";
+    	$raw = file_get_contents($url);
+    	$response = json_decode($raw);
+    	$description = array(
+    		'name' => utf8_decode($response->name),
+    		'class' => $response->class, 
+    		'gender' => $response->gender,
+    		'level' => $response->level,
+    		'thumbnail' => $response->thumbnail,
+    		'ilvl' => $response->items->averageItemLevelEquipped,
+    		);
+		$items = array(
+    		array ('name' => utf8_decode($response->items->head->name), 'id' => $response->items->head->id) ,
+    		array ('name' => utf8_decode($response->items->neck->name), 'id' => $response->items->neck->id ),
+    		array ('name' => utf8_decode($response->items->shoulder->name), 'id' => $response->items->shoulder->id ),
+    		array ('name' => utf8_decode($response->items->back->name), 'id' => $response->items->back->id ),
+    		array ('name' => utf8_decode($response->items->chest->name), 'id' => $response->items->chest->id ),
+    		array ('name' => utf8_decode($response->items->wrist->name), 'id' => $response->items->wrist->id ),
+    		array ('name' => utf8_decode($response->items->hands->name), 'id' => $response->items->hands->id ),
+    		array ('name' => utf8_decode($response->items->waist->name), 'id' => $response->items->waist->id ),
+    		array ('name' => utf8_decode($response->items->legs->name), 'id' => $response->items->legs->id ),
+    		array ('name' => utf8_decode($response->items->feet->name), 'id' => $response->items->feet->id ),
+    		array ('name' => utf8_decode($response->items->finger1->name), 'id' => $response->items->finger1->id ),
+    		array ('name' => utf8_decode($response->items->finger2->name), 'id' => $response->items->finger2->id ),
+    		array ('name' => utf8_decode($response->items->trinket1->name), 'id' => $response->items->trinket1->id ),
+    		array ('name' => utf8_decode($response->items->trinket2->name), 'id' => $response->items->trinket2->id )
+    		);
+		$url = "https://eu.api.battle.net/wow/character/Hyjal/".$character."?fields=stats&locale=fr_FR&apikey=gdqbrkpkzk4sbrs7ku3h2gxqwrax22zg";
+    	$raw = file_get_contents($url);
+    	$response = json_decode($raw);
+ 		$stats = array(
+    		'health' => $response->stats->health,
+    		'power' => $response->stats->power, 
+    		'str' => $response->stats->str,
+    		'agi' => $response->stats->agi,
+    		'int' => $response->stats->int,
+    		'crit' => $response->stats->crit,
+    		'haste' => $response->stats->haste,
+    		'mastery' => $response->stats->mastery,
+    		'versatility' => $response->stats->versatilityDamageDoneBonus
+    		);
+
+		var_dump($description);
+    	var_dump($items);
+    	var_dump($stats);
+        return $this->render('KWTBundle:Site:character.html.twig',array ('active'=>$active));
+    }
+
 }
